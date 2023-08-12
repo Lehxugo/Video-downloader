871521eba644dc4b93bfcacc8c058ddf74d9ab8f
Lehxugo <xavigo1234@gmail.com>
2023-08-12 20:55:16 +0200

    Home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,19 @@
+import { Link } from "react-router-dom";
+
 function Home () {
     return (
-        <h1>Home</h1>
+        <>
+            <h1 className="home-title">Video Downloader</h1>
+            <h2 className="home-subtitle">Download your favorite videos from your favorite platforms!</h2>
+            <div className="downloaders-grid">
+                <div className="downloader-cell">
+                    <Link to={'youtube'} className="downloader-card">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/120px-YouTube_full-color_icon_%282017%29.svg.png" alt="YouTube icon"></img>
+                        <p>YouTube</p>
+                    </Link>
+                </div>
+            </div>
+        </>
     );
 }
 
