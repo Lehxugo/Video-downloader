f21d5393ffec9cfdd5a1c83091ed5ebe89c5e0f6
Lehxugo <xavigo1234@gmail.com>
2023-08-21 10:45:36 +0200

    change downloaders container from grid to flex

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,8 +5,8 @@ function Home () {
         <>
             <h1 className="home-title">Video Downloader</h1>
             <h2 className="home-subtitle">Download your favorite videos from your favorite platforms!</h2>
-            <div className="downloaders-grid">
-                <div className="downloader-cell">
+            <div className="downloaders">
+                <div className="downloader">
                     <Link to={'youtube'} className="downloader-card">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/120px-YouTube_full-color_icon_%282017%29.svg.png" alt="YouTube icon"></img>
                         <p>YouTube</p>
